929d63d53fbe398472fd9dcdd3fb37e46a032c76
Svetsyn <svetsyn@gmail.com>
2021-02-08 14:35:12 +0200

    Open login panel!

--- judge/src/main/java/com/softuni/judge/repository/RoleRepository.java
+++ judge/src/main/java/com/softuni/judge/repository/RoleRepository.java
@@ -1,10 +1,15 @@
 package com.softuni.judge.repository;
 
 import com.softuni.judge.model.entity.Role;
+import com.softuni.judge.model.entity.RoleNameEnum;
 import jdk.jfr.Registered;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
+
+   Optional<Role> findByRoleNameEnum(RoleNameEnum roleNameEnum);
 }
--- judge/src/main/java/com/softuni/judge/web/UserController.java
+++ judge/src/main/java/com/softuni/judge/web/UserController.java
@@ -1,6 +1,10 @@
 package com.softuni.judge.web;
 
+import com.softuni.judge.model.binding.UserLoginBindingModel;
 import com.softuni.judge.model.binding.UserRegisterBiningModel;
+import com.softuni.judge.model.service.UserServiceModel;
+import com.softuni.judge.service.UserSevice;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,21 +14,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserSevice userSevice;
+    private final ModelMapper modelMapper;
+
+    public UserController(UserSevice userSevice, ModelMapper modelMapper) {
+        this.userSevice = userSevice;
+        this.modelMapper = modelMapper;
+    }
+
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+
+        if (!model.containsAttribute("userLoginBindingModel")){
+            model.addAttribute("userLoginBindingModel",new UserLoginBindingModel());
+            model.addAttribute("notFound", false);
+        }
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginConfirm(@Valid
+                               @ModelAttribute UserLoginBindingModel userLoginBindingModel,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes,
+                               HttpSession httpSession){
+
+        if (bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("userLoginBindingModel",userLoginBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userLoginBindingModel",bindingResult);
+            return "redirect:login";
+        }
+        UserServiceModel user = userSevice
+                .findUserByUsernameAndPassword(userLoginBindingModel.getUsername(),userLoginBindingModel.getPassword());
+
+        if (user==null){
+            redirectAttributes.addFlashAttribute("userLoginBindingModel",userLoginBindingModel);
+            redirectAttributes.addFlashAttribute("notFound",true);
+            return "redirect:login";
+        }
+        httpSession.setAttribute("user",user);
+
+        return "redirect:/";
     }
 
     @GetMapping("/register")
     public String register(Model model) {
-        if (!model.containsAttribute("userRedirectBindingModel")){
+        if (!model.containsAttribute("userRedirectBindingModel")) {
             model.addAttribute("userRedirectBindingModel", new UserRegisterBiningModel());
+
         }
         return "register";
     }
@@ -35,11 +79,16 @@ public class UserController {
             , BindingResult bindingResult
             , RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("userRedirectBindingModel",userRegisterBiningModel);
+            redirectAttributes.addFlashAttribute("userRedirectBindingModel", userRegisterBiningModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRedirectBindingModel", bindingResult);
             return "redirect:register";
         }
 
-        //ToDo save user in DB
+
+        UserServiceModel userServiceModel = modelMapper
+                .map(userRegisterBiningModel, UserServiceModel.class);
+
+        userSevice.registerUser(userServiceModel);
 
         return "redirect:login";
     }
